5f53db34231a9a9a83ba0d6f1d9899e671e27ea4
slavko lukić <111462208+slavkolukic@users.noreply.github.com>
2025-11-16 19:40:51 +0100

    feat: implement theme store persistance

--- source/core/hooks/useAppInit.ts
+++ source/core/hooks/useAppInit.ts
@@ -1,7 +1,9 @@
+import { useThemeStore } from "../store";
 import { useLoadFonts } from "./useLoadFonts";
 
 export const useAppInit = () => {
   const fontsLoaded = useLoadFonts();
+  const hasThemeStoreHydrated = useThemeStore((store) => store._hasHydrated);
 
-  return fontsLoaded;
+  return fontsLoaded && hasThemeStoreHydrated;
 };
--- source/core/store/useThemeStore.ts
+++ source/core/store/useThemeStore.ts
@@ -1,12 +1,34 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { ThemePreference } from "../types";
 
 type ThemeStore = {
   preference: ThemePreference;
   setPreference: (preference: ThemePreference) => void;
+  _hasHydrated: boolean;
+  setHasHydrated: (hasHydrated: boolean) => void;
 };
 
-export const useThemeStore = create<ThemeStore>((set) => ({
-  preference: "system",
-  setPreference: (preference) => set({ preference }),
-}));
+export const useThemeStore = create<ThemeStore>()(
+  persist(
+    (set) => ({
+      preference: "system",
+      setPreference: (preference) => set({ preference }),
+      _hasHydrated: false,
+      setHasHydrated: (state) => {
+        set({
+          _hasHydrated: state,
+        });
+      },
+    }),
+    {
+      name: "theme-preference",
+      storage: createJSONStorage(() => AsyncStorage),
+      partialize: (state) => ({ preference: state.preference }),
+      onRehydrateStorage: (state) => {
+        return () => state.setHasHydrated(true);
+      },
+    }
+  )
+);
